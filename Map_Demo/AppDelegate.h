044f422710c5e929bf85311fa8f5959b09ffd5bb
yq616775291 <616775291@qq.com>
2015-12-15 11:00:11 +0800

    Initial Commit

---
+++ Map_Demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Map_Demo
+//
+//  Created by yq on 15/12/15.
+//  Copyright © 2015年 yq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Map_Demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Map_Demo
+//
+//  Created by yq on 15/12/15.
+//  Copyright © 2015年 yq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
